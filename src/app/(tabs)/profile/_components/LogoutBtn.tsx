032e35b8ffb8492939cd073f44bdebb66353359b
chinmay <chinmayghulealpha@gmail.com>
2024-05-17 14:32:41 +0530

    Fixed the issue regarding meesages being fetched from the previous conversationId if the user logs out of an account and then logs in from another one. The stale conversationId used to persist and that would cause a bad request to be sent.

--- src/app/(tabs)/profile/_components/LogoutBtn.tsx
+++ src/app/(tabs)/profile/_components/LogoutBtn.tsx
@@ -1,22 +1,25 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { useConversationId } from "@/contexts/ActiveConversationId";
 import useLogout from "@/hooks/useLogout";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 function LogoutBtn() {
   const { loading, logoutResponse, logoutFn, error } = useLogout();
+  const { setConversationInfo } = useConversationId();
   const router = useRouter();
 
   // effects.
   useEffect(() => {
     if (logoutResponse) {
       alert(logoutResponse);
+      setConversationInfo(undefined);
       router.push("/login");
     }
     if (error) alert(error);
-  }, [error, logoutResponse, router]);
+  }, [error, logoutResponse, router, setConversationInfo]);
 
   return (
     <Button
